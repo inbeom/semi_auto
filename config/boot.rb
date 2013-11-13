45a66c14bb92b176aa212612eb7714c3ed0a624c
Inbeom Hwang <hwanginbeom@gmail.com>
2013-11-14 04:41:09 +0900

    Remove files no longer used anymore

--- config/boot.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift(File.join(File.dirname(__FILE__), '../lib'))
-ENV['RACK_ENV'] ||= 'development'
-
-require 'bundler'
-Bundler.setup
-Bundler.require(:default, ENV['RACK_ENV'].to_sym)
-
-require 'semi_auto'
