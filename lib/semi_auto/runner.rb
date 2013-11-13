5718dd713c92bcd5c1c7d08bb414bd1ca604420a
Inbeom Hwang <hwanginbeom@gmail.com>
2013-11-14 04:20:58 +0900

    Add match task

--- lib/semi_auto/runner.rb
+++ lib/semi_auto/runner.rb
@@ -1,12 +1,13 @@
 module SemiAuto
   class Runner
-    TASKS = { up: 'up', down: 'down' }
+    TASKS = { up: 'up', down: 'down', match: 'match' }
 
     def initialize(options = {})
       options[:config_file_path] ||= 'config/semi_auto.yml'
 
       @configuration = SemiAuto::Configuration.new(options)
-      @task = options[:task]
+      @task = options[:task].shift
+      @task_options = options[:task]
     end
 
     def run
@@ -32,9 +33,27 @@ module SemiAuto
         service.scale_up
       when TASKS[:down]
         service.scale_down
+      when TASKS[:match]
+        match_task
       end
 
       SemiAuto.logger.info "Current running instances - #{service.pool.running_instances.map(&:name).join(', ')}"
+    end
+
+    def match_task
+      target = @task_options.first.to_i
+
+      if service.pool.running_instances.count == target
+        SemiAuto.logger.info 'Nothing to do further'
+      elsif service.pool.running_instances.count < target
+        SemiAuto.logger.info 'Scaling up...'
+        service.scale_up
+        match_task
+      else
+        SemiAuto.logger.info 'Scaling down...'
+        service.scale_down
+        match_task
+      end
     end
   end
 end
